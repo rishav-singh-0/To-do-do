d63cff76540c51716b503a2fbefb0074a44bcf90
Rishav Singh <rsh04613@gmail.com>
2021-04-18 22:35:00 +0530

    added provider notifier at the top level of widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:record/screens/tasks_screen.dart';
+import 'package:record/models/task_data.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TaskScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => TaskData(),
+      child: MaterialApp(
+        home: TaskScreen(),
+      ),
     );
   }
 }
