b704293af411e95dfa6599b6b709b4a28b0595da
Rishav Singh <rsh04613@gmail.com>
2021-04-19 01:55:41 +0530

    update and delete taskdata

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -11,16 +11,27 @@ class TaskData extends ChangeNotifier {
     Task(name: 'Fill internship form', isDone: true),
   ];
 
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
+
   UnmodifiableListView get tasks {
     return UnmodifiableListView(_tasks);
   }
 
   int get taskCount {
     return _tasks.length;
-  }
-
-  void addTask(String newTaskTitle) {
-    final task = Task(name: newTaskTitle);
-    tasks.add(task);
   }
 }
