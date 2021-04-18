fd3cf6d31f953a69fb6627b98a196f617e4a1fb1
Rishav Singh <rsh04613@gmail.com>
2021-04-19 00:49:53 +0530

    made tasks list private to TaskData class

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,15 +1,21 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 // material package apready contains foundation package but using this to keep things minimal
 import 'package:record/models/tasks.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Make this todo list app', isDone: false),
     Task(name: 'Work on fire drone', isDone: false),
     Task(name: 'Fill internship form', isDone: true),
   ];
 
+  UnmodifiableListView get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 }
