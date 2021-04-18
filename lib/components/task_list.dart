42cc4ddf5a8f6bc77951c5bf84aac2682c83a38e
Rishav Singh <rsh04613@gmail.com>
2021-04-18 22:39:14 +0530

    Changes: - used provider package to read data - changed TaskList to stateless widget

--- lib/components/task_list.dart
+++ lib/components/task_list.dart
@@ -1,29 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:record/components/task_tile.dart';
-import 'package:record/models/tasks.dart';
+import 'package:record/models/task_data.dart';
 
-class TaskList extends StatefulWidget {
-  final List<Task> tasks;
-  TaskList(this.tasks);
-  @override
-  _TaskListState createState() => _TaskListState();
-}
-
-class _TaskListState extends State<TaskList> {
+class TaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-            taskTitle: widget.tasks[index].name,
-            isChecked: widget.tasks[index].isDone,
-            toggleCheckbox: (checkboxState) {
-              setState(() {
-                widget.tasks[index].toggleDone();
+    return Consumer<TaskData>(builder: (context, taskData, child) {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          return TaskTile(
+              taskTitle: taskData.tasks[index].name,
+              isChecked: taskData.tasks[index].isDone,
+              toggleCheckbox: (checkboxState) {
+                // setState(() {
+                //   widget.tasks[index].toggleDone();
+                // });
               });
-            });
-      },
-      itemCount: widget.tasks.length,
-    );
+        },
+        itemCount: taskData.taskCount,
+      );
+    });
   }
 }
