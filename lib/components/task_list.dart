8976b0ec9c0fb12269abd4295a2655cd88f2bcb3
Rishav Singh <rsh04613@gmail.com>
2021-04-19 01:54:41 +0530

    added functionality: - toggle checkbox - delete tasks by long pressing it

--- lib/components/task_list.dart
+++ lib/components/task_list.dart
@@ -9,14 +9,17 @@ class TaskList extends StatelessWidget {
     return Consumer<TaskData>(builder: (context, taskData, child) {
       return ListView.builder(
         itemBuilder: (context, index) {
+          final task = taskData.tasks[index];
           return TaskTile(
-              taskTitle: taskData.tasks[index].name,
-              isChecked: taskData.tasks[index].isDone,
-              toggleCheckbox: (checkboxState) {
-                // setState(() {
-                //   widget.tasks[index].toggleDone();
-                // });
-              });
+            taskTitle: task.name,
+            isChecked: task.isDone,
+            toggleCheckbox: (checkboxState) {
+              taskData.updateTask(task);
+            },
+            longPressCallback: () {
+              taskData.deleteTask(task);
+            },
+          );
         },
         itemCount: taskData.taskCount,
       );
